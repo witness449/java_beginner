11c7a10e2235f2f98b99a48a2606f2bbd51a429f
witness449 <lebedev_84@mail.ru>
2020-04-08 23:41:59 +0300

    Changing of source code folder Renaming packages

--- src/main/java/io/dima/javabeginner/console/Console.java
+++ src/main/java/io/dima/javabeginner/console/Console.java
@@ -4,11 +4,11 @@
  * and open the template in the editor.
  */
 
-package main.java.io.dima.javabeginner.console;
+package io.dima.javabeginner.console;
 
-import main.java.io.dima.javabeginner.domain.Card;
-import main.java.io.dima.javabeginner.domain.Column;
-import main.java.io.dima.javabeginner.domain.User;
+import io.dima.javabeginner.domain.Card;
+import io.dima.javabeginner.domain.Column;
+import io.dima.javabeginner.domain.User;
 import java.util.Scanner;
 
 /**
--- src/main/java/io/dima/javabeginner/domain/Column.java
+++ src/main/java/io/dima/javabeginner/domain/Column.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package main.java.io.dima.javabeginner.domain;
+package io.dima.javabeginner.domain;
 
 /**
  *
